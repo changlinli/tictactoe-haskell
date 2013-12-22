866005bffb3f959ea0bbd83cc56799ed25e9e9ab
Changlin <changlinli@college.harvard.edu>
2013-12-21 19:04:48 -0500

    Added new gitignore file and ExtendedNum type
    Added a new type for adding infinity to numbers and beginning work on an
    evaluation function.

--- negamax.hs
+++ negamax.hs
@@ -1,7 +1,14 @@
-data NegamaxTree a = EmptyTree | Node a [NegamaxTree a]
+data ExtendedNum a = Only a | NegInf | PosInf
+data NegamaxTree a state = EmptyTree | Node a state [NegamaxTree a state]
 
-singleton :: a -> NegamaxTree a
-singleton x = Node x [ EmptyTree ]
+singleton :: a -> state -> NegamaxTree a state
+singleton x state = Node x state [ EmptyTree ]
 
-{-evaluate :: (Ord a) => NegamaxTree a -> a-}
-{-evaluate (Node x ys) = foldl (\x (Node a bs) = max x a-}
+extendedNum2Num :: (Num a) => ExtendedNum a -> a
+extendedNum2Num NegInf = error "Things went wrong!"
+extendedNum2Num PosInf = error "Things went wrong!"
+extendedNum2Num (Only x) = x
+
+evaluate :: ((NegamaxTree (ExtendedNum Integer) state) -> ExtendedNum Integer) -> (NegamaxTree (ExtendedNum Integer) state) -> (ExtendedNum Integer)
+evaluate f EmptyTree = NegInf
+{-evaluate f (Node x state) = foldl (\x (Node a bs) -> max x a) x ys-}
