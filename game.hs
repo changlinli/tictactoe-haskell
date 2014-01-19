71b49b93239fc866efddd4228f3b3b84d7a21b17
Changlin Li <changlinli@college.harvard.edu>
2014-01-18 22:44:24 -0500

    Changed DS.Seq to List
    Using list of lists now as the main data type for the game board.
    Finished most of the logic for the game (not AI), now need to implement
    actually running the game.

--- game.hs
+++ game.hs
@@ -1,10 +1,11 @@
 import qualified Data.Sequence as DS
 import qualified Data.Foldable as DF
+import qualified Data.List as DL
 
 boardSize = 3
-data Players = Player1 | Player2 deriving Eq
+data Players = Player1 | Player2 deriving (Eq, Show)
 type GameBoardUnit = Maybe Players
-type GameBoard = DS.Seq (DS.Seq GameBoardUnit)
+type GameBoard = [[GameBoardUnit]]
 data GameState = PlayState { board :: GameBoard, currentPlayer :: Players } | Player1Win | Player2Win | Tie
 
 nextPlayer :: Players -> Players
@@ -12,13 +13,13 @@ nextPlayer Player1 = Player2
 nextPlayer Player2 = Player1
 
 isValidMove :: (Int, Int) -> GameBoard -> Bool
-isValidMove (a, b) board = if DS.index (DS.index board b) a == Nothing && a < boardSize && b < boardSize
+{-isValidMove (a, b) board = if DS.index (DS.index board b) a == Nothing && a < boardSize && b < boardSize-}
+isValidMove (a, b) board = if (board !! b) !! a == Nothing && a < boardSize && b < boardSize
            then True
            else False
 
 startingBoard :: GameBoard
-startingBoard = DS.fromList [DS.fromList [Nothing, Nothing, Nothing], DS.fromList [Nothing, Nothing, Nothing], DS.fromList [Nothing, Nothing, Nothing]]
--- startingBoard = [[Nothing, Nothing, Nothing], [Nothing, Nothing, Nothing], [Nothing, Nothing, Nothing]]
+startingBoard = [[Nothing, Nothing, Nothing], [Nothing, Nothing, Nothing], [Nothing, Nothing, Nothing]]
 
 startingState :: GameState
 startingState = PlayState { board=startingBoard, currentPlayer=Player1 }
@@ -32,33 +33,66 @@ playMove (a, b) (PlayState board player)
         | isValidMove (a, b) board = PlayState { board=newBoard, currentPlayer=nextPlayer player }
         | otherwise = error "Invalid Move!"
         where 
-                newBoard = DS.update b (DS.update a (Just player) (DS.index board b)) board
-
-blah = DS.fromList [DS.fromList [1, 2, 3], 
-                    DS.fromList [1, 2, 3], 
-                    DS.fromList [1, 2, 3]]
+                newBoard = updateBoard board (a, b) (Just player)
 
 -- TODO: Make diagonals more general
-diagonals :: GameBoard -> [[GameBoardUnit]]
-diagonals board = 
-        [
-                [DS.index (DS.index board 1) 1, DS.index (DS.index board 2) 2, DS.index (DS.index board 3) 3],
-                [DS.index (DS.index board 1) 3, DS.index (DS.index board 2) 2, DS.index (DS.index board 3) 1]
-        ]
+getDiagonals :: GameBoard -> [[GameBoardUnit]]
+getDiagonals [[ x1, _, x3 ],
+        [_, y1, _],
+        [z1, _, z3]] = [[x1, y1, z3], [x3, y1, z1]]
 
-rows :: GameBoard -> [[GameBoardUnit]]
-rows board = DF.toList $ fmap DF.toList board
+-- Yes I know, this does nothing, but it might if GameBoard changes from
+-- just being a list of lists
+getRows :: GameBoard -> [[GameBoardUnit]]
+getRows board = board
 
-{-transposeBoard :: GameBoard -> GameBoard-}
-{-transposeBoard board = DS.fromList [x | x <- DF.toList -}
+getCols :: GameBoard -> [[GameBoardUnit]]
+getCols board = transposeBoard board
 
-{-checkGameOver :: GameState -> GameState-}
-{-checkGameOver PlayState board _ = any-}
+transposeBoard :: GameBoard -> GameBoard
+transposeBoard board = DL.transpose board
 
-checkDiagonal :: GameBoard -> Maybe Players
-checkDiagonal board 
-        | [Just Player1, Just Player1, Just Player1] `elem` diagonals board = Just Player1
-        | [Just Player2, Just Player2, Just Player2] `elem` diagonals board = Just Player2
+checkRows :: GameBoard -> Maybe Players
+checkRows board = checkThreeInARow (getRows board)
+
+checkCols :: GameBoard -> Maybe Players
+checkCols board = checkThreeInARow (getCols board)
+
+checkGameOver :: GameState -> GameState
+checkGameOver (PlayState board player)
+        | checkDiagonals board == Just Player1 = Player1Win
+        | checkDiagonals board == Just Player2 = Player2Win
+        | checkRows board == Just Player1 = Player1Win
+        | checkRows board == Just Player2 = Player2Win
+        | checkCols board == Just Player1 = Player1Win
+        | checkCols board == Just Player2 = Player2Win
+        | checkDraw board = Tie
+        | otherwise = PlayState {board=board, currentPlayer=player}
+
+checkDraw :: GameBoard -> Bool
+checkDraw [row] 
+        | Nothing `elem` row = False
+        | otherwise = True
+checkDraw (x:xs)
+        | not (checkDraw [x]) = checkDraw [x]
+        | otherwise = checkDraw xs
+
+updateList :: [a] -> a -> Int -> [a]
+updateList (x:xs) newItem index
+        | index == 0 = newItem:xs
+        | otherwise = x:(updateList xs newItem (index - 1))
+
+updateBoard :: GameBoard -> (Int, Int) -> GameBoardUnit -> GameBoard
+updateBoard oldBoard (x, y) value = updateList oldBoard (updateList (oldBoard !! y) value x) y
+
+checkThreeInARow :: [[GameBoardUnit]] -> Maybe Players
+checkThreeInARow board
+        | [Just Player1, Just Player1, Just Player1] `elem` getDiagonals board = Just Player1
+        | [Just Player2, Just Player2, Just Player2] `elem` getDiagonals board = Just Player2
         | otherwise = Nothing
 
-rpt = cycle
+checkDiagonals :: GameBoard -> Maybe Players
+checkDiagonals board = checkThreeInARow (getDiagonals board)
+
+showGameBoard :: GameBoard -> String
+showGameBoard board = show board
