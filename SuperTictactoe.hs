237aec5062b5554e0c2e34cf3e55c6ea3b24824c
Changlin Li <changlinli@college.harvard.edu>
2014-03-31 04:49:00 -0400

    Finished game over checks
    Functions necessary for determining whether a supertictactoe game is
    over have been finished.

--- SuperTictactoe.hs
+++ SuperTictactoe.hs
@@ -2,30 +2,31 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE OverlappingInstances #-}
 
-boardSize = 3
+import qualified Negamax as Negamax
+import qualified Tictactoe as Tic
+import qualified Data.List as DL
+import qualified Control.Applicative as CA
+
+boardSize = Tic.boardSize
 superBoardSize = 3
-data Players = Player1 | Player2 deriving (Eq, Show)
-type GameBoardUnit = Maybe Players
 
--- The use of all the pragmas was to justify this syntactic sugar of the
--- use of show. If we don't want the pragmas, just make a new function
--- instead of using show to display the game board
-instance Show GameBoardUnit where
-        show (Just Player1) = "X"
-        show (Just Player2) = "O"
-        show Nothing = " "
+-- Note that this is hugely inefficient to just have the gameboard. Ideally
+-- we would also include which player has won certain subboards of our
+-- super board so we don't have to recalculate every time
+type SuperGameBoard = [[ Tic.GameBoard ]]
+data SuperGameState = SuperPlayState { currentSuperBoard :: SuperGameBoard, currentSuperPlayer :: Tic.Players } | Player1WinSuper | Player2WinSuper | TieSuper deriving (Show, Eq)
 
-type GameBoard = [[GameBoardUnit]]
-data GameState = PlayState { board :: GameBoard, currentPlayer :: Players } | Player1Win | Player2Win | Tie deriving (Show, Eq)
+startingSuperBoard :: SuperGameBoard
+startingSuperBoard = [[Tic.startingBoard, Tic.startingBoard], [Tic.startingBoard, Tic.startingBoard], [Tic.startingBoard, Tic.startingBoard]]
 
-type SuperGameBoard = [[ GameBoard ]]
-data SuperGameState = SuperPlayState { currentSuperBoard :: SuperGameBoard, currentSuperPlayer :: Players } | Player1WinSuper | Player2WinSuper | TieSuper deriving (Show, Eq)
+startingSuperState :: SuperGameState
+startingSuperState = SuperPlayState { currentSuperBoard=startingSuperBoard, currentSuperPlayer=Tic.Player1 }
 
-nextPlayer :: Players -> Players
-nextPlayer Player1 = Player2
-nextPlayer Player2 = Player1
+nextPlayer :: Tic.Players -> Tic.Players
+nextPlayer Tic.Player1 = Tic.Player2
+nextPlayer Tic.Player2 = Tic.Player1
 
-getGameBoardUnit :: ((Int, Int), (Int, Int)) -> SuperGameBoard -> GameBoardUnit
+getGameBoardUnit :: ((Int, Int), (Int, Int)) -> SuperGameBoard -> Tic.GameBoardUnit
 getGameBoardUnit ((a, b), (c, d)) board = (((board !! b) !! a) !! d) !! c
 
 isValidMove :: ((Int, Int), (Int, Int)) -> SuperGameBoard -> Bool
@@ -33,3 +34,33 @@ isValidMove coordinates@((a, b), (c, d)) board =
         if getGameBoardUnit coordinates board  == Nothing && c < boardSize && d < boardSize && a < superBoardSize && b < superBoardSize
            then True
            else False
+
+evalFunc :: SuperGameState -> Negamax.ExtendedNum Integer
+evalFunc = undefined
+
+checkSuperFull :: SuperGameBoard -> Bool
+checkSuperFull superBoard = and (map foldRow superBoard)
+        where foldRow = foldl (\x y -> Tic.checkFull y && x) True
+
+checkSuperThreeInARow :: [[ Tic.GameBoard ]] -> Maybe Tic.Players
+checkSuperThreeInARow listOfRows = True `DL.elemIndex` (map Tic.sameEntries listOfWins) >>= (\n -> head (listOfWins !! n))
+        where listOfWins = map (map Tic.findWinner) listOfRows
+
+checkSuperRows :: SuperGameBoard -> Maybe Tic.Players
+checkSuperRows superBoard = checkSuperThreeInARow (Tic.getRows superBoard)
+
+checkSuperCols :: SuperGameBoard -> Maybe Tic.Players
+checkSuperCols superBoard = checkSuperThreeInARow (Tic.getCols superBoard)
+
+checkSuperDiagonals :: SuperGameBoard -> Maybe Tic.Players
+checkSuperDiagonals superBoard = checkSuperThreeInARow (Tic.getDiagonals superBoard)
+
+findSuperWinner :: SuperGameBoard -> Maybe Tic.Players
+findSuperWinner superBoard = checkSuperDiagonals superBoard CA.<|> checkSuperRows superBoard CA.<|> checkSuperCols superBoard
+
+checkSuperGameOver :: SuperGameState -> SuperGameState
+checkSuperGameOver (SuperPlayState superBoard superPlayer)
+        | findSuperWinner superBoard == Just Tic.Player1 = Player1WinSuper
+        | findSuperWinner superBoard == Just Tic.Player2 = Player2WinSuper
+        | checkSuperFull superBoard = TieSuper
+        | otherwise = SuperPlayState {currentSuperBoard = superBoard, currentSuperPlayer = superPlayer}
