2b1723c130df8b4ec2c9b41b98e95ce0cbba7731
Changlin Li <changlinli@college.harvard.edu>
2014-04-14 03:51:03 -0400

    Removed matrix transposition.
    Matrix transposition in order to grab the columns of a board was taking
    up a lot of performance and so I've swapped it out for just finding the
    columns for 3x3 matrices (in a way that is not scalable to matrices of
    other sizes).

--- Tictactoe.hs
+++ Tictactoe.hs
@@ -63,7 +63,11 @@ getRows :: [[a]] -> [[a]]
 getRows board = board
 
 getCols :: [[a]] -> [[a]]
-getCols board = transposeBoard board
+getCols
+        [[ x1, x2, x3],
+        [y1, y2, y3],
+        [z1, z2, z3]] = [[x1, y1, z1], [x2, y2, z2], [x3, y3, z3]]
+{-getCols board = transposeBoard board-}
 
 transposeBoard :: [[a]] -> [[a]]
 transposeBoard board = DL.transpose board
